93299341817c1795c0bde5382e4af37def151147
Jon Johansen <jonjohansen2k@gmail.com>
2020-01-15 20:58:54 +0100

    Add authorization to swagger doc

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,19 +1,30 @@
 from flask_restplus import Api
 from .members import api as members
-from .servers import api as servers
-from .events import api as events
+# from .servers import api as servers
+# from .events import api as events
+from .auth import api as auth
 
 
+authorizations = {
+    'Bearer Token': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization',
+        'description': 'Bearer token in format: [Bearer TOKEN]'
+    }
+}
 # General about API
 api = Api(
     title='TDCTL-API',
     version='0.0',
     description='''TDCTL-database API.
     Everything related to Tromsøstudentenes Dataforening''',
-    contact='td@list.uit.no'
+    contact='td@list.uit.no', authorizations=authorizations
 )
+
 
 # Register namespaces
 api.add_namespace(members)
-api.add_namespace(servers)
-api.add_namespace(events)
+# api.add_namespace(servers)
+# api.add_namespace(events)
+api.add_namespace(auth)
