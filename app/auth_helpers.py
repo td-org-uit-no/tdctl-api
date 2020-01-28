12f33322bd095e99d3a2c6671bb2b1897b72df5f
Jon Johansen <jonjohansen2k@gmail.com>
2020-01-28 03:58:57 +0100

    Auth helpers - Refresh tokens
    - Type the functions for easier use
    - Refresh tokens are now managed
      - Refresh tokens have functions for blacklisting

--- app/auth_helpers.py
+++ app/auth_helpers.py
@@ -1,28 +1,50 @@
-from flask import request
+from flask import request, current_app as app
 from functools import wraps
-from flask import current_app as app
 from flask_restplus import Namespace
 from datetime import datetime, timedelta
 from werkzeug.exceptions import Unauthorized, Forbidden
+
 from jwt import encode, decode, ExpiredSignatureError, DecodeError
+from uuid import uuid4
+from .db import mongo
 
 
-def create_token(user):
+def create_token(user: dict):
     payload = {
         # Token lifetime
         'exp': datetime.utcnow() + timedelta(days=0, minutes=60),
         'iat': datetime.utcnow(),
         'user_id': user['id'],
-        'roles': user['roles']
+        'roles': user['roles'],
+        'access_token': True        # Separates it from refresh token
     }
-    return encode(
-        payload,
-        app.config.get('SECRET_KEY'),
-        algorithm='HS256'
-    )
+    return encode(payload, app.config.get('SECRET_KEY'), algorithm='HS256')
 
 
-def authorize(request):
+def create_refresh_token(user: dict):
+    return encode({
+        # Token lifetime
+        "exp": datetime.utcnow() + timedelta(days=0, minutes=60),  # Expiry
+        "iat": datetime.utcnow(),                                  # Issued at
+        "jti": uuid4().hex,                                        # Token id
+        "user_id": user['id']
+    }, app.config.get('SECRET_KEY'), algorithm='HS256')
+
+
+def decode_token(token: bytes):
+    try:
+        # Attempt to decode the token found in header
+        return decode(token.encode('utf-8'), app.config.get('SECRET_KEY'))
+    except DecodeError:
+        # Missing segments
+        raise Unauthorized('Token has incorrect format.')
+    except ExpiredSignatureError:
+        raise Unauthorized('Token has expired.')
+    except:  # noqa: E722
+        raise Unauthorized('Unknown error')
+
+
+def authorize(request: request):
     '''
     Takes a request and goes through all the steps of authenticating it.
     On valid request, function will return the payload.
@@ -42,26 +64,35 @@ def authorize(request):
     if (tokens[0] != 'Bearer'):
         raise Unauthorized('Token prefix is incorrect.')
 
-    try:
-        # Attempt to decode the token found in header
-        return decode(tokens[1].encode('utf-8'), app.config.get('SECRET_KEY'))
-    except DecodeError:
-        # Missing segments
-        raise Unauthorized('Token has incorrect format.')
-    except ExpiredSignatureError:
-        raise Unauthorized('Token has expired.')
-    except:  # noqa: E722
-        raise Unauthorized('Unknown error')
+    token = decode_token(tokens[1])
+    if not token['access_token']:
+        raise Unauthorized(
+            'The token is not an access token. Is this a refresh token?')
+    return token
 
 
-def invalidate_token(token):
-    # We need to invalidate the token somehow
-    # And potentially clean the list of invalid tokens
-    pass
+def blacklist_token(refreshToken: dict):
+    '''
+    Blacklists the provided (decoded) refresh token.
+    '''
+    # Insert token into database
+    mongo.db.tokens.insert_one(
+        {
+            # We have to parse the timestamps into date object for mongodb
+            "exp": datetime.utcfromtimestamp(refreshToken['exp']),
+            "iat": datetime.utcfromtimestamp(refreshToken['iat']),
+            "jti": refreshToken["jti"],
+            "user_id": refreshToken["user_id"]
+        })
+    # Return something?
 
 
-def is_blacklisted(token):
-    # Checks the validity of a token
+def is_blacklisted(refreshToken: dict):
+    '''
+    Check if the provided (decoded) refresh token is blacklisted.
+    '''
+    if (mongo.db.tokens.find_one({"jit": refreshToken["jit"]})):
+        return True
     return False
 
 
@@ -71,7 +102,7 @@ or as separate roles before continuing the logic in this decorator
 '''
 
 
-def role_required(api, roles):
+def role_required(api: Namespace, roles: list):
     '''
     Decorator to aid with authorization of a resource.
 
@@ -101,7 +132,7 @@ def role_required(api, roles):
     return decorate
 
 
-def login_required(api):
+def login_required(api: Namespace):
     '''
     Decorator to aid with validation and interpretation of tokens.
 
