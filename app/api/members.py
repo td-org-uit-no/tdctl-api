943d2a52cc1335cb50febc2e83ce12c3c4270178
Jon Johansen <jonjohansen2k@gmail.com>
2020-01-28 04:00:52 +0100

    Remodelling and fix creation of members
    - Models have slightly better names
    - Creation of members registers and returns the right fields
    	- This includes password management

--- app/api/members.py
+++ app/api/members.py
@@ -1,20 +1,22 @@
 from flask_restplus import Namespace, Resource
 from werkzeug.exceptions import NotFound, Conflict
+from werkzeug.security import generate_password_hash
+from uuid import uuid4
 
 from ..db import mongo
-from ..models import PartialMember, Member as _member
+from ..models import PartialMember, Member as _Member
 from ..auth_helpers import login_required, role_required
 
 api = Namespace('member', description="interfaces to interact with members")
 
 # Register model
 api.models['PartialMember'] = PartialMember
-api.models['Member'] = _member
+api.models['Member'] = _Member
 
 
 @api.route('/<int:id>')
 class Member(Resource):
-    @api.marshal_with(_member)
+    @api.marshal_with(_Member)
     @api.expect(id, validate=True)
     @role_required(api, 'Member')
     @api.response(404, "Not found: User not found")
@@ -30,21 +32,32 @@ class Member(Resource):
 class Member(Resource):
 
     @api.expect(PartialMember, validate=True)
-    @api.marshal_with(_member)
+    @api.marshal_with(_Member)
     @api.response(409, "Conflict: E-mail is already in use.")
     def post(self):
         '''Creates a new member. Returns the complete object.'''
+        existingUser = mongo.db.members.find_one(
+            {'email': api.payload['email'].lower()})
+        if existingUser:
+            raise Conflict('E-mail is already in use.')
+
+        # Build the new member
+        newMember = api.payload
+        newMember['email'] = newMember['email'].lower()  # Lowercase e-mail
+        newMember['_id'] = uuid4()  # Generate ID
+        newMember['password'] = generate_password_hash(api.payload['password'])
+        newMember['STATUS'] = 'INACTIVE'  # Set default status for new members
+
+        res = mongo.db.members.insert_one(newMember)  # Create the user!
+
         # TODO:
-        # * Check if e-mail already exists
-        #   * Return a 409: Conflict if it does.
-        # * Clean information: lowercase the email, string trailing spaces etc
-        # * Assign id properly.
-        # * Hash the password.
-        api.payload['id'] = 1
-        res = mongo.db.members.insert_one(api.payload)
+        #   * E-mail confirmation
+        #   * Password requirements
+        #   * Some tiny e-mail validation (Mostly handled by confirmation)
+
         return mongo.db.members.find_one(res.inserted_id)
 
-    @api.marshal_with(_member)
+    @api.marshal_with(_Member)
     @login_required(api)
     def get(self, token):
         '''Returns a user object associated with token in header'''
@@ -53,7 +66,7 @@ class Member(Resource):
 
 @api.route('s/')  # noqa: F811  # Redef error
 class Member(Resource):
-    @api.marshal_list_with(_member)
+    @api.marshal_list_with(_Member)
     @login_required(api)
     def get(self):
         '''List all members objects'''
