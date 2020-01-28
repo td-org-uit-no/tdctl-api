654d286c3475672f2628ef15f5f7689c3ba34a6c
Jon Johansen <jonjohansen2k@gmail.com>
2020-01-28 04:00:52 +0100

    Remodelling and fix creation of members
    - Models have slightly better names
    - Creation of members registers and returns the right fields
    	- This includes password management

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from flask_restplus import Model, fields
 
 # Used as a base model to create member
 PartialMember = Model('PartialMember', {
-    # Should be changed to name? We dont work with nicknames
     'realName': fields.String(required=True),
     'email': fields.String(required=True),
     'password': fields.String(required=True),
@@ -11,19 +10,23 @@ PartialMember = Model('PartialMember', {
     'phone': fields.String
 })
 
-# Used to respond with a detailed model
-Member = PartialMember.clone('Member', {
-    'id': fields.Integer,
-    '_id': fields.String,
+# Used to represent the member
+Member = Model('Member', {
+    '_id': fields.String(required=True),
+    'realName': fields.String(required=True),
+    'email': fields.String(required=True),
+    'classof': fields.String(required=True),
+    'graduated': fields.Boolean(required=True),
+    'phone': fields.String,
     'roles': fields.List(fields.String),
     'status': fields.String,
 })
 
-loginModel = Model('loginModel', {
+Login = Model('Login', {
     'email': fields.String(required=True),
     'password': fields.String(required=True)})
 
-tokenModel = Model('tokenModel', {
-    'token': fields.String,
-    'refreshToken': fields.String
-})
+
+RefreshToken = Model('RefreshToken', {'refreshToken': fields.String})
+
+Tokens = RefreshToken.clone('Tokens', {'token': fields.String})
