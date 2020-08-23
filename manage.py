6a20192bff2549e0ab75339008b1d3a1f884e258
Jon Johansen <jonjohansen2k@gmail.com>
2020-08-23 16:15:11 +0200

    :whale: Modify manage.py to properly build right environments

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-
+import sys
 from app import create_app
 
-app = create_app('production')
+def mainEntry(environment='development'):
+    return create_app(environment)
+
+# Gunicorn entry point generator
+def appEntry():
+    return mainEntry(environment='production')
 
 if __name__ == "__main__":
+    app = mainEntry()
     app.run(host='0.0.0.0')
+
