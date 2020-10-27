cfcd191e3d930a21eefc3928a842db52edc3b5ad
Jon Johansen <jonjohansen2k@gmail.com>
2020-10-27 06:04:05 +0100

    :card_file_box: Database is now gotten through dependency injection instead of global instance

--- app/db.py
+++ app/db.py
@@ -1,2 +1,11 @@
-from flask_pymongo import PyMongo
-mongo = PyMongo()
+from pymongo import MongoClient, database
+from fastapi import Request
+
+
+def get_database(request: Request) -> database:
+    return request.app.db
+
+
+def setup_db(app):
+    app.db = MongoClient(app.config.MONGO_URI)[app.config.MONGO_DBNAME]
+    db = app.db
