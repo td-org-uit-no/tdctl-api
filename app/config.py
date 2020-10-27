3324c92077fbb9ad4d3522bc83ab1c0c19471838
Jon Johansen <jonjohansen2k@gmail.com>
2020-10-27 06:02:11 +0100

    :truck: Move config resources into app and create base class

--- app/config.py
+++ app/config.py
@@ -1,17 +1,25 @@
 import os
 
 
-class DevelopmentConfig:
+class Config:
+    SECRET_KEY: str
+    ENV: str
+    MONGO_HOST: str
+    MONGO_PORT: int
+    MONGO_DBNAME: str
+    MONGO_URI: str
+
+
+class DevelopmentConfig(Config):
     SECRET_KEY = "crashtest"
     ENV = 'development'
-    DEBUG = True  # Sets 'hot-reloading'
     MONGO_HOST = "127.0.0.1"
     MONGO_PORT = 27018
     MONGO_DBNAME = "tdctl"
     MONGO_URI = "mongodb://%s:%s/%s" % (MONGO_HOST, MONGO_PORT, MONGO_DBNAME)
 
 
-class ProductionConfig:
+class ProductionConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     ENV = 'production'
     MONGO_HOST = os.environ.get('DB_HOSTNAME')
