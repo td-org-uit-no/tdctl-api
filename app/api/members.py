cf5761f6bce2c3338fe5e993b028955744d06cc8
Jon Johansen <jonjohansen2k@gmail.com>
2020-10-31 15:23:52 +0100

    :bug: Fix minor bug in creation of members

--- app/api/members.py
+++ app/api/members.py
@@ -13,55 +13,58 @@ router = APIRouter()
 @router.post('/', tags=["member"])
 def create_new_member(request: Request, newMember: MemberInput):
     '''
-        TODO:
-            * E-mail confirmation
-            * Currently returns a ConfirmationCode for development purposes
-            * Password requirements
+        TODO:\n
+            - E-mail confirmation
+            - Currently returns a ConfirmationCode for development purposes
+            - Password requirements
     '''
     db = get_database(request)
     exists = db.members.find_one({'email': newMember.email.lower()})
     if exists:
         raise HTTPException(409, 'E-mail is already in use.')
-    member = MemberDB.parse_obj(newMember.dict())
-    member.id = uuid4().hex  # Generate ID
-    member.email = newMember.email.lower(),  # Lowercase e-mail
-    member.password = generate_password_hash(newMember.password),
-    member.role = 'unconfirmed',
-    member.status = 'inactive',
+    additionalFields = {
+        'id': uuid4().hex,  # Generate ID
+        'email': newMember.email.lower(),  # Lowercase e-mail
+        'password': generate_password_hash(newMember.password),
+        'role': 'unconfirmed',
+        'status': 'inactive',
+    }
 
+    member = newMember.dict()
+    member.update(additionalFields)
     # Create user object
-    db.members.insert_one(member.dict())
+    db.members.insert_one(member)
 
     # Create confirmation code
-    confirmationCode = uuid4().hex
+    confirmationCode=uuid4().hex
     db.confirmations.insert_one(
-        {"confirmationCode": confirmationCode, 'user_id': newMember.id}
+        {"confirmationCode": confirmationCode, 'user_id': member['id']}
     )
     return confirmationCode
 
 
-@router.get('/', tags=["member"])
-def get_member_associated_with_token(request: Request, token: AccessTokenPayload = Depends(authorize)):
-    db = get_database(request)
-    currentMember = db.members.find_one({'id': token.user_id})
+@ router.get('/', tags=["member"])
+def get_member_associated_with_token(request: Request, token: AccessTokenPayload=Depends(authorize)):
+    db=get_database(request)
+    currentMember=db.members.find_one({'id': token.user_id})
     if not currentMember:
         raise HTTPException(404, "User could not be found")
     return Member.parse_obj(currentMember)
 
 
-@router.get('/{id}', tags=["member"], response_model=Member, responses={404: {"model": None}})
-def get_member_by_id(request: Request, id: str, token: dict = Depends(authorize)):
+@ router.get('/{id}', tags=["member"], response_model=Member, responses={404: {"model": None}})
+def get_member_by_id(request: Request, id: str, token: dict=Depends(authorize)):
     '''Returns a user object associated with id passed in'''
-    db = get_database(request)
-    member = db.members.find_one({'id': id})
+    db=get_database(request)
+    member=db.members.find_one({'id': id})
     if not member:
         raise HTTPException(404, 'Member not found')
     return Member.parse_obj(member)
 
 
-@router.get("s/", tags=["member"], response_model=List[Member])
-def get_all_members(request: Request, token: AccessTokenPayload = Depends(authorize)):
+@ router.get("s/", tags=["member"], response_model=List[Member])
+def get_all_members(request: Request, token: AccessTokenPayload=Depends(authorize)):
     '''List all members objects'''
     role_required(token, 'admin')
-    db = get_database(request)
+    db=get_database(request)
     return [Member.parse_obj(m) for m in db.members.find()]
