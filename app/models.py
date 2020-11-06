0089cd2fd32a8bad3d3f06233d792ae36669cdd1
Jon Johansen <jonjohansen2k@gmail.com>
2020-11-07 00:11:13 +0100

    :fire: Remove old models

--- app/models.py
+++ app/models.py
@@ -4,41 +4,6 @@ from uuid import uuid4
 from werkzeug.security import generate_password_hash
 
 
-'''
-# Used as a base model to create member
-PartialMember = Model('PartialMember', {
-    'realName': fields.String(required=True),
-    'email': fields.String(required=True),
-    'password': fields.String(required=True),
-    'classof': fields.String(required=True),
-    'graduated': fields.Boolean(required=True),
-    'phone': fields.String
-})
-
-# Used to represent the member
-Member = Model('Member', {
-    '_id': fields.String(required=True),
-    'realName': fields.String(required=True),
-    'email': fields.String(required=True),
-    'classof': fields.String(required=True),
-    'graduated': fields.Boolean(required=True),
-    'phone': fields.String,
-    'role': fields.String,
-    'status': fields.String,
-})
-
-Login = Model('Login', {
-    'email': fields.String(required=True),
-    'password': fields.String(required=True)})
-
-RefreshToken = Model('RefreshToken', {'refreshToken': fields.String})
-
-Tokens = RefreshToken.clone('Tokens', {'token': fields.String})
-
-ConfirmationCode = Model('ConfirmationCode', {'confirmationCode': fields.String})
-'''
-
-
 class AccessTokenPayload(BaseModel):
     exp: int
     iat: int
@@ -52,6 +17,7 @@ class RefreshTokenPayload(BaseModel):
     iat: int
     jti: str
     user_id: str
+
 
 class MemberInput(BaseModel):
     realName: str
@@ -89,9 +55,16 @@ class Tokens(BaseModel):
     accessToken: str
     refreshToken: str
 
+
 class RefreshToken(BaseModel):
     refreshToken: str
+
 
 class Credentials(BaseModel):
     email: EmailStr
     password: str
+
+
+class ChangePasswordPayload(BaseModel):
+    password: str
+    newPassword: str
