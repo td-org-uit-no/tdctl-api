b57345104b84e65c3200649da7db863d1fa62002
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2022-05-09 16:07:34 +0200

    :seedling: add seeding for users and events
    Co-authored-by: BerntJulian <Berntjulian@gmail.com>

--- db/seed.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-for file in `ls seeds`; do
-	echo "Seeding $file"
-	collection=`echo $file | cut -d"." -f1`
-	mongoimport --port 27018 --db tdctl --collection $collection --file seeds/$file --jsonArray
-	echo ""
-done
