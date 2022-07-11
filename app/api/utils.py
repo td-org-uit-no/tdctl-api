de58b7ec56bff66272eaea7ee2f73f7c994ba61d
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2022-07-11 21:51:57 +0200

    :lock: add UUID typing

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Response, Request, HTTPException, Depends
 from uuid import UUID
 
-def crud_get_event_by_id(db, eid: str):
-    event = db.events.find_one({'eid': UUID(eid).hex})
+def crud_get_event_by_id(db, eid: UUID):
+    event = db.events.find_one({'eid': eid.hex})
 
     if not event:
         raise HTTPException(404, "Event could not be found")
