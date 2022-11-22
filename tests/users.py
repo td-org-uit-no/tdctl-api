a06b5a2b651a2336b2bed47e7a7dfd01aa7a5738
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2022-11-22 14:25:05 +0100

    :recycle: move all tests user into one file

---
+++ tests/users.py
@@ -0,0 +1,20 @@
+regular_member = {
+    "email": "test@test.com",
+    "password": "Test!234"
+}
+
+second_member = {
+    "email": "first@lastname.com",
+    "password": "Test!234"
+}
+
+second_admin = {
+    "email": "second_admin@test.com",
+    "password": "&AdminTester1"
+}
+
+admin_member = {
+    "email": "test_admin@test.com",
+    "password": "&AdminTester1"
+}
+
