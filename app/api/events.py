8852fab5dec89f3d3583f46d96ddfa6d5c46de3e
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-01-30 11:03:56 +0100

    :sparkles: add penalty registration for late cancellation and disallow joins before registration is open

--- app/api/events.py
+++ app/api/events.py
@@ -4,11 +4,13 @@ import shutil
 from fastapi import APIRouter, Response, Request, HTTPException, Depends
 from fastapi.datastructures import UploadFile
 from fastapi.param_functions import File
+from pydantic import ValidationError
 from starlette.responses import FileResponse
 from starlette.background import BackgroundTasks
 from uuid import uuid4, UUID
+from app.utils.event_utils import should_penalize, valid_registration, validate_event_dates
 from app.utils.validation import validate_image_file_type, validate_uuid
-from ..auth_helpers import authorize, authorize_admin
+from ..auth_helpers import authorize, authorize_admin, optional_authentication
 from ..db import get_database, get_image_path, get_export_path
 from ..models import Event, EventDB, AccessTokenPayload, EventInput, EventUpdate, JoinEventPayload, Participant
 from .utils import get_event_or_404
@@ -19,57 +21,67 @@ router = APIRouter()
 
 @router.post('/')
 def create_event(request: Request, newEvent: EventInput, token: AccessTokenPayload = Depends(authorize_admin)):
-    db = get_database(request)
     # TODO better format handling and date date-time handling
-    try:
-        event_date = datetime.strptime(str(newEvent.date), "%Y-%m-%d %H:%M:%S")
-    except ValueError:
-        raise HTTPException(400, "Invalid date format")
-
-    if event_date < datetime.now():
-        raise HTTPException(400, "Invalid date")
+    db = get_database(request)
+    # validates event date and registrationOpningDate
+    validate_event_dates(newEvent)
+    
+    member = db.members.find_one({'id': UUID(token.user_id)})
+    if member == None:
+        raise HTTPException(500, "Problem caller not found")
+    host_email =  member["email"]
 
     eid = uuid4()
     additionalFields = {
         'eid': eid,
         'participants': [],
         'posts': [],
+        'registeredPenalties': [],
+        'host': host_email
     }
 
     event = newEvent.dict()
     event.update(additionalFields)
-
     event = db.events.insert_one(event)
 
     return {'eid': eid.hex}
 
 
 @router.get('/')
-def get_all_event_ids(request: Request):
+def get_all_event_ids(request: Request, token: AccessTokenPayload = Depends(optional_authentication)):
+    '''
+    returns all public events and if user is admin all events are returned
+    '''
     db = get_database(request)
-
-    return [str(event['eid']) for event in db.events.find()]
+    search_filter = {"public": {"$eq": True}}
+    if token and token.role == "admin":
+        search_filter = {}
+    return [str(event['eid']) for event in db.events.find(search_filter)]
 
 
 @router.get('/upcoming')
-def get_upcoming_events(request: Request):
+def get_upcoming_events(request: Request, token: AccessTokenPayload = Depends(optional_authentication)):
+    '''
+    Provides public upcoming events to regular members and return all events to admin
+    '''
     db = get_database(request)
-
     now = datetime.now()
     try:
         date_str = now.strftime("%Y-%m-%d %H:%M:%S")
         date = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
     except ValueError:
         raise HTTPException(500, "Problem handling date format")
+    # search filter for non admin users
+    search_filter = {"$and": [{'date': {'$gt': date}}, {"public": {"$eq": True}}]}
+    if token and token.role == "admin":
+        search_filter={'date': {"$gt": date}}
 
-    coming_events = db.events.find({'date': {'$gt': date}})
+    upcoming_events = db.events.find(search_filter)
 
-    return [Event.parse_obj(event) for event in coming_events]
+    return [Event.parse_obj(event) for event in upcoming_events]
 
 # custom uuid validation as eid: UUID will not allow users to copy eids into swagger as they are not formatted correctly
 # id is used over eid as parameter name as validate_uuid and the api function needs the have the same parameter name.
-
-
 @router.get('/{id}/image', dependencies=[Depends(validate_uuid)])
 def get_event_picture(request: Request, id: str):
     image_path = get_image_path(request)
@@ -86,7 +98,6 @@ def upload_event_picture(request: Request, id: str, image: UploadFile = File(...
 
     image_path = get_image_path(request)
 
-    # baseDir = "db/eventImages"
     if not os.path.isdir(image_path):
         os.mkdir(image_path)
 
@@ -100,9 +111,15 @@ def upload_event_picture(request: Request, id: str, image: UploadFile = File(...
 
 @router.put('/{id}', dependencies=[Depends(validate_uuid)])
 def update_event(request: Request, id: str, eventUpdate: EventUpdate, AccessTokenPayload=Depends(authorize_admin)):
+    """ To unset an optional field set the value to null """
     db = get_database(request)
+    event = get_event_or_404(db, id)
+    
+    # exclude_unset allows null to be included allowing for optional fields to be updated to be null i.e not present
+    values = eventUpdate.dict(exclude_unset=True)
+    if len(values) == 0:
+        raise HTTPException(400, "Update values cannot be empty")
 
-    values = eventUpdate.dict(exclude_none=True)
     if "date" in values:
         try:
             event_date = datetime.strptime(
@@ -113,21 +130,34 @@ def update_event(request: Request, id: str, eventUpdate: EventUpdate, AccessToke
         if event_date < datetime.now():
             raise HTTPException(400, "Invalid date")
 
+    # check if update does not remove required fields i.e a valid update
+    try:
+        EventDB.parse_obj({**event, **values})
+    except ValidationError:
+        raise HTTPException(400, "Cannot remove field as this is required filed for all events")
+
     result = db.events.find_one_and_update(
         {'eid': UUID(id)},
         {"$set": values})
 
-    if result == None:
-        raise HTTPException(404, "No such event with this eid")
+    if not result:
+        raise HTTPException(500, "Unexpected error when updating event")
 
     return Response(status_code=200)
 
 
 @router.get('/{id}', dependencies=[Depends(validate_uuid)])
-def get_event_by_id(request: Request, id: str):
+def get_event_by_id(request: Request, id: str, token: AccessTokenPayload = Depends(optional_authentication)):
     db = get_database(request)
-
     event = get_event_or_404(db, id)
+    role = None
+
+    if token:
+        role = token.role
+    if event["public"] == False and role != "admin":
+        # only allow admin members acces to unpublished events
+        raise HTTPException(403, "Insufficient privileges to access this resource")
+
     return EventDB.parse_obj(event)
 
 
@@ -149,6 +179,14 @@ def join_event(request: Request, id: str, payload: JoinEventPayload, token: Acce
 
     if not member:
         raise HTTPException(400, "User could not be found")
+
+    # admin can join all events
+    if member["role"] != "admin":
+        if not valid_registration(event["registrationOpeningDate"]):
+            raise HTTPException(403, "Event registration is not open")
+
+        if event["public"] == False:
+            raise HTTPException(403, "Event is not public")
 
     check_participant = db.events.find_one({"eid": event["eid"]}, {"participants": {
         "$elemMatch": {"id": member["id"]}}})
@@ -184,6 +222,7 @@ def leave_event(request: Request, id: str, token: AccessTokenPayload = Depends(a
     db = get_database(request)
     event = get_event_or_404(db, id)
     member = db.members.find_one({'id': UUID(token.user_id)})
+    penalty = should_penalize(event, token.user_id)
 
     if not member:
         raise HTTPException(400, "User could not be found")
@@ -198,8 +237,20 @@ def leave_event(request: Request, id: str, token: AccessTokenPayload = Depends(a
     except KeyError:
         raise HTTPException(400, "User not joined event!")
 
+    if penalty:
+        res = db.events.update_one(
+            {'eid': event["eid"]}, 
+            {"$addToSet": {"registeredPenalties": member["id"]}}
+        )
+        # only give penalty if addToSet added a new entry
+        if res.modified_count != 0:
+            db.members.find_one_and_update(
+                {'id': member["id"]},
+                {"$inc": {'penalty': 1}}
+            )
+
     db.events.update_one({'eid': event['eid']}, {
-        "$pull": {"participants": {"id": token.user_id}}})
+        "$pull": {"participants": {"id": member["id"]}}})
 
     if event['maxParticipants']:
         if len(event['participants']) >= event['maxParticipants']:
@@ -333,7 +384,6 @@ async def exportEvent(background_tasks: BackgroundTasks, request: Request, id: s
                 worksheet.set_row(
                     col+1, 15 + 1, cell_format=participants_wait_format2)
     # Cleanup file created after request is done
-    #writer.save()
     writer.close()
     background_tasks.add_task(os.remove, path)
 
