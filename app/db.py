ee16b3712e8825b63378338facefc27f894e2c1a
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-02-09 10:37:12 +0000

    :bug: fix double slash in image path

--- app/db.py
+++ app/db.py
@@ -23,15 +23,15 @@ def get_export_path(request: Request) -> str:
 def setup_db(app):
     app.db = MongoClient(app.config.MONGO_URI, uuidRepresentation="standard")[
         app.config.MONGO_DBNAME]
-    app.export_path = 'db/eventExports/'
+    app.export_path = 'db/eventExports'
 
     # Expire reset password codes after 10 minutes
     app.db.passwordResets.create_index("createdAt", expireAfterSeconds=60 * 10)
     if app.config.MONGO_DBNAME == 'test':
         app.image_path = 'db/testEventImages'
         return
-    app.image_path = 'db/eventImages/'
-    app.jobImage_path = 'db/jobImages/'
+    app.image_path = 'db/eventImages'
+    app.jobImage_path = 'db/jobImages'
 
 
 def get_test_db():
