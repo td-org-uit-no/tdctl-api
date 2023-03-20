ddc02afeadc00dad0f6f479589c08e861e23fa7d
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-03-20 14:47:47 +0100

    :sparkles: parse event in get_event_or_404

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,10 +1,12 @@
 from fastapi import HTTPException
 from uuid import UUID
 
+from app.models import EventDB
+
 def get_event_or_404(db, eid: str):
     event = db.events.find_one({'eid': UUID(eid)})
 
     if not event:
         raise HTTPException(404, "Event could not be found")
 
-    return event
+    return EventDB.parse_obj(event).dict()
